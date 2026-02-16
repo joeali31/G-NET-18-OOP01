d2a0cccf51fbb0f9803e2ac0ba54e26507f3a6f0
joeali31 <ya9415565@gmail.com>
2026-02-16 07:17:33 -0800

    Question01

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -6,7 +6,27 @@
         {
 
             #region Question01
+            //Explain with code example how class and struct behave differently
 
+            /* struct
+             * type : Value type
+             * Stored In : Stack
+             * Assignment : Copies the data
+             * Inheritance :  Not supported
+             * Default Constructor : Always exists, cannot be removed
+             * Null : Cannot be null (unless nullable)
+             * Best For : Small, simple data
+             */
+
+            /* Class
+             * type : Reference type
+             * Stored In : Heap (reference on stack)
+             * Assignment : Copies the reference
+             * Inheritance : Supported
+             * Default Constructor : Provided if none defined
+             * Null : Can be null
+             * Best For : Complex entities with behavior
+             */
             #endregion
 
         }
